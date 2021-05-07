183fffb7566e93ed5bee8f764457fc23f9389dda
dushyanta-pimpalkar <49636406+dushyanta-pimpalkar@users.noreply.github.com>
2021-05-07 16:08:49 +0530

    Add files via upload

---
+++ ListUser/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './components/list-page/list.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'list', component: ListComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
